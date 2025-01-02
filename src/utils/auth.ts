7b12492b37d98bc53f3b938151c39da3fb7b108b
Egge <egge@getcurrent.io>
2025-01-02 10:58:02 +0100

    added jwt verification

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,5 @@
 import { nip19, nip98 } from "nostr-tools";
+import jwt from "jsonwebtoken";
 import { AuthData } from "../types";
 
 export async function verifyAuth(
@@ -8,22 +9,44 @@ export async function verifyAuth(
   body?: any,
 ): Promise<AuthData> {
   try {
-    let isValid: boolean;
-    const event = await nip98.unpackEventFromToken(authHeader).catch((err) => {
-      throw err;
-    });
-    if (Boolean(body) && Object.keys(body).length > 0) {
-      isValid = await nip98.validateEvent(event, url, method, body);
+    if (authHeader.startsWith("Nostr") || authHeader.startsWith("nostr")) {
+      let isValid: boolean;
+      const event = await nip98
+        .unpackEventFromToken(authHeader)
+        .catch((err) => {
+          throw err;
+        });
+      if (Boolean(body) && Object.keys(body).length > 0) {
+        isValid = await nip98.validateEvent(event, url, method, body);
+      } else {
+        isValid = await nip98.validateEvent(event, url, method);
+      }
+      if (!isValid) {
+        return { authorized: false };
+      }
+      return {
+        authorized: true,
+        data: { pubkey: event.pubkey, npub: nip19.npubEncode(event.pubkey) },
+      };
+    } else if (authHeader.startsWith("Bearer")) {
+      const [_, token] = authHeader.split(" ");
+      const parsedHeader = jwt.verify(token, process.env.JWT_SECRET!) as {
+        pubkey: string;
+      };
+      if (!parsedHeader.pubkey) {
+        return { authorized: false };
+      }
+      const data = {
+        authorized: true,
+        data: {
+          pubkey: parsedHeader.pubkey,
+          npub: nip19.npubEncode(parsedHeader.pubkey),
+        },
+      };
+      return data;
     } else {
-      isValid = await nip98.validateEvent(event, url, method);
-    }
-    if (!isValid) {
       return { authorized: false };
     }
-    return {
-      authorized: true,
-      data: { pubkey: event.pubkey, npub: nip19.npubEncode(event.pubkey) },
-    };
   } catch (e) {
     return { authorized: false };
   }
